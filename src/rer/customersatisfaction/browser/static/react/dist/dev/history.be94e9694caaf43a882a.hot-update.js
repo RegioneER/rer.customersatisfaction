d2537a9fbff3858290da299dd29524815938e8d5
Nicola Zambello <nzambello@protonmail.com>
2021-06-29 09:53:31 +0200

    refactor: show feedbacks and viewlet

--- src/rer/customersatisfaction/browser/static/react/dist/dev/history.be94e9694caaf43a882a.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate("history",[])
-//# sourceMappingURL=history.be94e9694caaf43a882a.hot-update.js.map
--- src/rer/customersatisfaction/browser/static/react/dist/dev/history.js
+++ src/rer/customersatisfaction/browser/static/react/dist/dev/history.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c150c601ee50fe29fd6d";
+/******/ 	var hotCurrentHash = "1e8ded280c2c10efc8cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
