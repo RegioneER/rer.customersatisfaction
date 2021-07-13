a8661e6a132d18f097a6190167a46b738ae51fce
Nicola Zambello <nzambello@protonmail.com>
2021-07-13 12:24:11 +0200

    fix: active status on viewlet buttons

--- src/rer/customersatisfaction/browser/static/rer-customersatisfaction.js
+++ src/rer/customersatisfaction/browser/static/rer-customersatisfaction.js
@@ -1,102 +1,103 @@
-require(["jquery"], function ($) {
-  "use strict";
+require(['jquery'], function($) {
+  'use strict';
 
   function addErrorMessage(err) {
     const resp = err.responseJSON;
     if (!resp) {
       return;
     }
-    $("#customer-satisfaction").prepend(
+    $('#customer-satisfaction').prepend(
       '<div class="customer-satisfaction-message portalMessage error" role="alert"><strong>' +
         resp.message +
-        '</strong><button class="plone-btn plone-btn-link delete-message" title="Elimina messaggio">&times;</button></div>'
+        '</strong><button class="plone-btn plone-btn-link delete-message" title="Elimina messaggio">&times;</button></div>',
     );
   }
 
   function doSuccessCleanup() {
-    $("#customer-satisfaction").prepend(
-      '<div class="customer-satisfaction-message portalMessage info" role="alert"><strong>Grazie per il tuo feedbak</strong><button class="plone-btn plone-btn-link delete-message" title="Elimina messaggio">&times;</button></div>'
+    $('#customer-satisfaction').prepend(
+      '<div class="customer-satisfaction-message portalMessage info" role="alert"><strong>Grazie per il tuo feedbak</strong><button class="plone-btn plone-btn-link delete-message" title="Elimina messaggio">&times;</button></div>',
     );
     $('#customer-satisfaction form').remove();
   }
 
   function cleanMessage() {
-    $(".customer-satisfaction-message").remove();
+    $('.customer-satisfaction-message').remove();
   }
 
   function cleanVotes() {
-    $("#customer-satisfaction form label").each(function () {
-      $(this).removeClass("active");
-      $(this).find("input[type='radio']").prop("checked", false);
+    $('#customer-satisfaction form label').each(function() {
+      $(this).removeClass('active');
+      $(this)
+        .find('input[type="radio"]')
+        .prop('checked', false);
     });
   }
 
   function setVoteActive(label) {
-    label.addClass("active");
-    label.find("input[type='radio']").prop("checked", true);
-    label.find("input[type='radio']").select();
+    label.addClass('active');
+    label.find('input[type="radio"]').prop('checked', true);
+    label.find('input[type="radio"]').select();
   }
 
   function expandCollapse(expand) {
-    var collapse = $("#cs-collapsible-form-area");
+    var collapse = $('#cs-collapsible-form-area');
     if (expand) {
-      collapse.attr("aria-expanded", true);
+      collapse.attr('aria-expanded', true);
     } else {
-      collapse.attr("aria-expanded", false);
+      collapse.attr('aria-expanded', false);
     }
   }
 
-  $("#customer-satisfaction form input[type='radio']").on(
-    "click",
-    function (event) {
-      var currentLabel = $(event.target).closest("label");
-      if (currentLabel && !currentLabel.hasClass("active")) {
-        cleanVotes();
-        setVoteActive(currentLabel);
-        expandCollapse(true);
-      } else {
-        cleanVotes();
-        expandCollapse(false);
-      }
+  $('#customer-satisfaction form input[type="radio"]').on('click', function(
+    event,
+  ) {
+    var currentLabel = $(event.target).closest('label');
+    if (currentLabel && !currentLabel.hasClass('active')) {
+      cleanVotes();
+      setVoteActive(currentLabel);
+      expandCollapse(true);
+    } else {
+      cleanVotes();
+      expandCollapse(false);
     }
-  );
+  });
 
-  $("#customer-satisfaction form").on("submit", function (event) {
+  $('#customer-satisfaction form').on('submit', function(event) {
     var form = $(this);
     event.preventDefault();
     cleanMessage();
     var data = {};
     form
-      .find(":input")
+      .find(':input')
       .serializeArray()
-      .forEach(function (param) {
+      .forEach(function(param) {
         data[param.name] = param.value;
       });
     $.ajax({
-      type: "POST",
-      contentType: "application/json",
-      url: form.attr("action"),
+      type: 'POST',
+      contentType: 'application/json',
+      url: form.attr('action'),
       data: JSON.stringify(data),
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       processData: false,
-      success: function (_msg) {
-        form.trigger("reset");
+      success: function() {
+        form.trigger('reset');
         doSuccessCleanup();
       },
-      error: function (err) {
+      error: function(err) {
         addErrorMessage(err);
       },
     });
   });
 
-  $("#customer-satisfaction").on(
-    "click",
-    ".customer-satisfaction-message button.delete-message",
-    function () {
+  $('#customer-satisfaction').on(
+    'click',
+    '.customer-satisfaction-message button.delete-message',
+    function() {
       cleanMessage();
-    }
+    },
   );
 });
